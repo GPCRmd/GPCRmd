f76fed257bdda827a8094d0fdc35e97fb7da0bc9
palilsonjmr <palilson.jmr@gmail.com>
2016-07-07 17:13:36 +0200

    javascript increase protein number

--- dynadb/static/dynadb/addProtein.js
+++ dynadb/static/dynadb/addProtein.js
@@ -1,7 +1,15 @@
-
+var l;
+l = 1;
 function addProtein() {
+    l += 1;
+    var ll; 
+    ll = l-1; 
+    var protnumb= "PROTEIN  #" + l;
+    var item = document.getElementById("protform");
+    var t = item.cloneNode(true);
+    var idlabnod= "protform" + l;
+    t.id=idlabnod;
+    t.childNodes[1].childNodes[1].childNodes[1].innerHTML=protnumb;
+    document.getElementById("pprotform").appendChild(t)[ll];
 
-var item = document.getElementById("protform");  
-var t = item.cloneNode(true);      
-document.getElementById("pprotform").appendChild(t)[1];
 }
