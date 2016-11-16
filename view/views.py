7a1ef283f9d41cf0978bcb9da266759406ca9727
marionatf <mariona_tf@hotmail.com>
2016-11-16 17:57:37 +0100

    comparison between conserved positions of different classes added

--- view/views.py
+++ view/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from dynadb.models import DyndbFiles, DyndbFilesDynamics, DyndbModelComponents, DyndbCompound, DyndbDynamicsComponents
+from dynadb.models import DyndbFiles, DyndbFilesDynamics, DyndbModelComponents, DyndbCompound, DyndbDynamicsComponents,DyndbDynamics
 from view.assign_generic_numbers_from_DB import obtain_gen_numbering
 from dynadb.pipe4_6_0 import *
 from view.data import *
@@ -26,8 +26,8 @@ def find_range_from_cons_pos(my_pos, gpcr_pdb):
         if n > len(pdb_positions_inchain):  
             return False
     return pos_range
- 
-def create_conserved_pos_list(gpcr_pdb,gpcr_aa, i,my_pos, cons_pos_li, add,multiple_chains,chain_name):
+                              
+def create_conserved_pos_list(gpcr_pdb,gpcr_aa, i,my_pos, cons_pos_li, multiple_chains,chain_name):
     """Given the GPCR num of a position of our seq, checks if it's one of the conserved residues, and if it has been mutated"""
     my_pos_bw=my_pos.split("x")[0]
     add_chain_name=""
@@ -44,6 +44,21 @@ def create_conserved_pos_list(gpcr_pdb,gpcr_aa, i,my_pos, cons_pos_li, add,multi
                 (my_aa,chain)=gpcr_aa[my_pos]
                 if my_aa != cons_aa:
                     cons_pos_li[i][0]=cons_pos+my_aa
+        i+=1
+                                    
+def create_conserved_pos_list_otherclass(gpcr_pdb,gpcr_aa, i,my_pos, cons_pos_li, multiple_chains,chain_name,gnum_classes_rel,dict_class,current_class):
+    """Given the GPCR num of a position of our seq, checks if it's one of the conserved residues, and if it has been mutated"""
+    my_pos_bw=my_pos.split("x")[0]
+    add_chain_name=""
+    if multiple_chains:
+        add_chain_name=":"+chain_name
+    while i < len(cons_pos_li):
+        cons_pos_bw_ourclass = cons_pos_li[i][1]
+        if my_pos_bw==cons_pos_bw_ourclass:
+            pos_range=find_range_from_cons_pos(my_pos, gpcr_pdb)
+            if pos_range:
+                cons_pos_li[i][2]=pos_range + add_chain_name
+                cons_pos_li[i][1]=cons_pos_bw_ourclass +  current_class.lower()
         i+=1
 
 def create_conserved_motif_list(gpcr_pdb,gpcr_aa,j,my_pos,motifs,multiple_chains,chain_name):
@@ -66,15 +81,36 @@ def create_conserved_motif_list(gpcr_pdb,gpcr_aa,j,my_pos,motifs,multiple_chains
                     motifs[j][1]+=my_aa
         j+=1
 
-def find_missing_pos(cons_pos_li):
+def create_conserved_motif_list_otherclass(gpcr_pdb,gpcr_aa,j,my_pos,motifs,multiple_chains,chain_name):
+    """Given the GPCR num of a position of our seq, checks if it's one of the residues of a conserved motif, and if it has been mutated"""
+    my_pos_bw=my_pos.split("x")[0]
+    (my_aa,chain)=gpcr_aa[my_pos]
+    add_chain_name=""
+    if multiple_chains:
+        add_chain_name=":"+chain_name    
+    while j < len(motifs):
+        cons_pos_bw = motifs[j][4]
+        if my_pos_bw==cons_pos_bw:
+            pos_range=find_range_from_cons_pos(my_pos, gpcr_pdb)
+            if pos_range:
+                motifs[j][2]=True
+                motifs[j][3]=pos_range + add_chain_name
+        j+=1
+
+
+def find_missing_pos(cons_pos_li, alt_class=False):
     """Given a list of conserved positions, where the positions found at our sequence are indicated, modifies the list to indicate the positions not found."""
-    for e in cons_pos_li:
-        if e[2]=="":
-            i=cons_pos_li.index(e)
+    i=0
+    while i < len(cons_pos_li):
+        if cons_pos_li[i][2]=="":
             cons_pos_li[i][2]="None"
+        # if alt_class:
+        #     cons_pos_li[i][0] += alt_class.lower() 
+        i+=1
+
 
 def find_missing_pos_in_motif(motifs, motname_li):
-    """Given a list of positions in conserved motifs, where the positions found at our sequence are indicated, creates a list of motifs where the positions found at our sequence, are indicated, showing if they have the conserves AA or another one."""
+    """Given a list of positions in conserved motifs, where the positions found at our sequence are indicated, creates a list of motifs where the positions found at our sequence are indicated, showing if they have the conserves AA or another one."""
     motifs_def=[]
     n=0
     for motname in motname_li:
@@ -99,6 +135,35 @@ def find_missing_pos_in_motif(motifs, motname_li):
             motifs_def.append([motname,(" , ".join(not_found)+" not found"),(ranges_all)])
         n+=1
     return motifs_def
+
+def find_missing_pos_in_motif_otherclass(motifs, motname_li,dict_class,current_class):
+    """Given a list of positions in conserved motifs, where the positions found at our sequence are indicated, creates a list of motifs where the positions found at our sequence are indicated, showing if they have the conserves AA or another one."""
+    motifs_def=[]
+    n=0
+    for motname in motname_li:
+        found=[]
+        found_ranges=[]
+        not_found=[]
+        for e in motifs:
+            if e[0]==motname:
+                if not e[2]:
+                    not_found.append(e[1])
+                else:
+                    motpos= e[1] + current_class.lower() + " = " + e[4] +dict_class.lower()
+                    found.append(motpos)
+                    found_ranges.append(e[3])
+                   
+        num_nf=len(not_found)
+        ranges_all=",".join(found_ranges)
+        if num_nf ==0:
+            motifs_def.append([motname," ; ".join(found),(ranges_all)])
+        elif num_nf ==3:
+            motifs_def.append([motname,"Motif not found","None"])
+        else:
+            motifs_def.append([motname,(" ; ".join(found) + " (" + " , ".join(not_found) + " not found)"),(ranges_all)])
+        n+=1
+    return motifs_def
+
 
 def obtain_dyn_files(paths_list):
     """Given a list of files related to a dynamic, separates them in structure files and trajectory files."""
@@ -128,31 +193,95 @@ def obtain_prot_chains(pdb_name):
             chain_name_li.append(chain.id)
     return chain_name_li
 
-def obtain_predef_positions_lists(result,numbers,mol_sw,cons_classA,motifs,gpcr_pdb,gpcr_aa,multiple_chains,chain_name):
-    """Takes the predefined lists of positions/motifs that will appear as predefined views and modifies them so that they match the sequence of our protein."""
+def obtain_rel_dicts(result,numbers,chain_name,current_class):
+    """Creates a series of dictionaries that will be useful for relating the pdb position with the gpcr number (pos_gnum) or AA (pos_gnum); and the gpcr number for the different classes (in case the user wants to compare)"""
+    gpcr_pdb={}
+    gpcr_aa={}
+    gnum_classes_rel={}
+    pos_gnum = numbers[current_class]
     for pos in result:
         if pos[0] != "-": #Consider only num in the pdb
             db_pos=pos[1][1]
-            if numbers[db_pos][1]: #If exist GPCR num for this position
-                gpcr_pdb[numbers[db_pos][1]]=[pos[0][2],chain_name]
-                gpcr_aa[numbers[db_pos][1]]=[numbers[db_pos][0], chain_name]
+            if pos_gnum[db_pos][1]: #If exist GPCR num for this position
+                gpcr_pdb[pos_gnum[db_pos][1]]=[pos[0][2],chain_name]
+                gpcr_aa[pos_gnum[db_pos][1]]=[pos_gnum[db_pos][0], chain_name]
+    other_classes=list({"A","B","C","F"} - set(current_class))
+    other_classes_ok=[]
+    for name in other_classes:
+        if numbers[name]:
+            other_classes_ok.append(name)
+            gnum_classes_rel[name]={}
+    for pos, (res,gnum) in pos_gnum.items():
+        if gnum:
+            for class_name in other_classes_ok:
+                gnum_altclass=numbers[class_name][pos][1]
+                if gnum_altclass:
+                    gnum_classes_rel[class_name][gnum_altclass.split("x")[0]]=gnum.split("x")[0]
+    return(gpcr_pdb,gpcr_aa,gnum_classes_rel,other_classes_ok)
 
+def traduce_all_poslists_to_ourclass_numb(motifs_dict,gnum_classes_rel,cons_pos_dict,current_class,other_classes_ok):
+    """Takes all the lists of conserved residues and traduces to the GPCR numbering of the class of the protein to visualize the conserved positions of the rest of classes."""
+    current_poslists=cons_pos_dict[current_class]
+    current_motif = motifs_dict[current_class]
+    show_class={"A":True,"B":True,"C":True,"F":True}
+    for gpcr_class in other_classes_ok:
+        for cons_pos_li in cons_pos_dict[gpcr_class]:
+            for el in cons_pos_li:
+                pos_nm=el[0]
+                s=re.search("([A-Z]?)([\d\.]+)",pos_nm)
+                AA=s.group(1)
+                bw_pos_ok=s.group(2)
+                current_bw_pos=gnum_classes_rel[gpcr_class][bw_pos_ok]
+                el[0]=AA + bw_pos_ok + gpcr_class.lower()
+                el[1]=current_bw_pos
+        motif_info = motifs_dict[gpcr_class]
+        if motif_info:
+            for el in motif_info[0]:
+                bw_pos=el[1][1:]
+                current_bw_pos=gnum_classes_rel[gpcr_class][bw_pos]
+                el[4]=current_bw_pos
+    none_classes=list({"A","B","C","F"} - set(other_classes_ok + list(current_class)))
+    for n_class in none_classes:
+        show_class[n_class]=False
+        i=0
+        while i < len(cons_pos_dict[n_class]):
+            cons_pos_dict[n_class][i]=None
+            i+=1
+        motifs_dict[n_class]=[]
+    return (show_class,current_poslists,current_motif,other_classes_ok)
+
+
+def obtain_predef_positions_lists(current_poslists,current_motif,other_classes_ok,current_class,cons_pos_dict,motifs,gpcr_pdb,gpcr_aa,gnum_classes_rel,multiple_chains,chain_name):
+    """Takes the predefined lists of positions/motifs that will appear as predefined views and modifies them so that they match the sequence of our protein."""
     # Obtain list for the predefined views of important positions
     chain_pos = [pos for pos in gpcr_aa if gpcr_aa[pos][1]==chain_name]
     for my_pos in chain_pos:
-        i=id_start_pos=0
-        create_conserved_pos_list(gpcr_pdb, gpcr_aa,i,my_pos,mol_sw, id_start_pos,multiple_chains,chain_name)
-        id_start_pos2=len(mol_sw)
-        create_conserved_pos_list(gpcr_pdb, gpcr_aa,i,my_pos,cons_classA, id_start_pos2,multiple_chains,chain_name)
-        j=0
-        create_conserved_motif_list(gpcr_pdb,gpcr_aa,j,my_pos,motifs,multiple_chains,chain_name)
+        for cons_pos_li in current_poslists:
+            create_conserved_pos_list(gpcr_pdb, gpcr_aa,0,my_pos,cons_pos_li,multiple_chains,chain_name)
+        if current_motif:
+            create_conserved_motif_list(gpcr_pdb,gpcr_aa,0,my_pos,current_motif[0],multiple_chains,chain_name)
+        for gpcr_class in other_classes_ok:
+            for cons_pos_li in cons_pos_dict[gpcr_class]:                     
+                create_conserved_pos_list_otherclass(gpcr_pdb,gpcr_aa, 0,my_pos, cons_pos_li, multiple_chains,chain_name,gnum_classes_rel,gpcr_class,current_class)
+            alt_class_motif=motifs_dict[gpcr_class]
+            if alt_class_motif:
+                create_conserved_motif_list_otherclass(gpcr_pdb,gpcr_aa,0,my_pos,alt_class_motif[0],multiple_chains,chain_name)
 
-def find_missing_positions(mol_sw,cons_classA,motifs):
-    "Completes the conserved position lists to indicate the positions that are missing in our prot"
-    find_missing_pos(mol_sw)
-    find_missing_pos(cons_classA)
-    motifs_def=find_missing_pos_in_motif(motifs, motname_li)
-    return motifs_def
+
+def find_missing_positions(motifs_dict_def,current_motif,current_poslists,other_classes_ok,current_class,cons_pos_dict,motifs):
+    """Completes the conserved position lists to indicate the positions that are missing in our prot"""
+    for cons_pos_li in current_poslists:
+        find_missing_pos(cons_pos_li)
+    if current_motif:
+        motifs_def=find_missing_pos_in_motif(current_motif[0], current_motif[1])
+        motifs_dict_def[current_class] = motifs_def
+    for dict_class in other_classes_ok:
+        for cons_pos_li in cons_pos_dict[dict_class]:
+            find_missing_pos(cons_pos_li,dict_class)
+        alt_class_motif=motifs_dict[dict_class]
+        if alt_class_motif:
+            motifs_def=find_missing_pos_in_motif_otherclass(motifs, motname_li,dict_class,current_class)
+            motifs_dict_def[dict_class] = motifs_def
 
 def obtain_compounds(dyn_id):
     """Creates a list of the ligands, ions, lipids, water molecules, etc found at the dynamic"""
@@ -175,8 +304,24 @@ def obtain_compounds(dyn_id):
     comp_li=sorted(comp_li, key=lambda x: x[0])
     return(comp_li)
 
-def index(request):
-    dyn_id =1 #EXAMPLE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def findGPCRclass(num_scheme):
+    """Uses the numbering scheme name to determine the GPCR family (A, B, C or F). Also sets the values of a dict that will determine the class shown at the template."""
+    if num_scheme == "gpcrdba" or num_scheme == "gpcrdb":
+        current_class ="A"
+        active_class["A"]=["active","in active"]
+    elif num_scheme == "gpcrdbb":
+        current_class ="B"
+        active_class["B"]=["active","in active"]
+    elif num_scheme == "gpcrdbc":
+        current_class ="C"
+        active_class["C"]=["active","in active"]
+    elif num_scheme == "gpcrdbf":
+        current_class ="F"
+        active_class["F"]=["active","in active"]
+    return current_class
+
+def index(request, dyn_id):
+    #dyn_id =1 #EXAMPLE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     dynfiles=DyndbFilesDynamics.objects.prefetch_related("id_files").filter(id_dynamics=dyn_id)
     if len(dynfiles) ==0:
         error="Structure file not found."
@@ -193,38 +338,54 @@ def index(request):
         chain_str=""
         if len(chain_name_li) > 1:
             multiple_chains=True
-            chain_str="(Chains: "+", ".join(chain_name_li)+")"        
-        if len(obtain_gen_numbering(dyn_id))==3:
-            (numbers, num_scheme, db_seq) = obtain_gen_numbering(dyn_id) 
+            chain_str="(Chains: "+", ".join(chain_name_li)+")"
+        gen_num_res=obtain_gen_numbering(dyn_id)
+        if isinstance(gen_num_res, tuple):
+            (numbers, num_scheme, db_seq, current_class) = gen_num_res
+            current_class=findGPCRclass(num_scheme)
+            # current_class="B" ######## !!!!!!!!!!!!!!!!!!!!!!!!! REMOVE THIIIS!!!!!!!!!!!!!!!!!!!
             gpcr_n_ex=""
-            if "gpcr" in num_scheme: 
-                for e in numbers:
-                    gpcr_n_ex= numbers[e][1]
-                    if gpcr_n_ex: #We take the 1st instance of gpcr num as example, and check in which format it is (n.nnxnn or nxnn)
-                        break
-                if "." in gpcr_n_ex: #For the moment we only accept n.nnxnn format
-                    gpcr_pdb={}
-                    gpcr_aa={}
-                    for chain_name in chain_name_li:
-                        checkpdb_res=checkpdb(pdb_name, segid="",start=-1,stop=99999, chain=chain_name)
-                        if isinstance(checkpdb_res, tuple):
-                            tablepdb,pdb_sequence,hexflag=checkpdb_res
-                            result=matchpdbfa(db_seq,pdb_sequence, tablepdb, hexflag)
-                            if isinstance(result, list):
-                                obtain_predef_positions_lists(result,numbers,mol_sw,cons_classA,motifs,gpcr_pdb,gpcr_aa,multiple_chains,chain_name)                                              
-                    motifs_def=find_missing_positions(mol_sw,cons_classA,motifs)
-                    gpcr_pdb_js=json.dumps(gpcr_pdb) 
-                    context={
-                        "structure_file":structure_file, 
-                        "structure_name":structure_name , 
-                        "traj_list":traj_list, 
-                        "compounds" : comp_li,
-                        "mol_sw" : mol_sw,
-                        "cons_classA" : cons_classA,
-                        "motifs_def" : motifs_def,
-                        "chains" : chain_str,
-                        "gpcr_pdb": gpcr_pdb_js}
-                    return render(request, 'view/index.html', context)
+            for pos_gnum in numbers[current_class].values():
+                if pos_gnum[1]: #We take the 1st instance of gpcr num as example, and check in which format it is (n.nnxnn or nxnn)
+                    gpcr_n_ex=pos_gnum[1]
+                    break
+            if "." in gpcr_n_ex: #For the moment we only accept n.nnxnn format
+                for chain_name in chain_name_li:
+                    checkpdb_res=checkpdb(pdb_name, segid="",start=-1,stop=99999, chain=chain_name)
+                    if isinstance(checkpdb_res, tuple):
+                        tablepdb,pdb_sequence,hexflag=checkpdb_res
+                        result=matchpdbfa(db_seq,pdb_sequence, tablepdb, hexflag)
+                        if isinstance(result, list):
+                            (gpcr_pdb,gpcr_aa,gnum_classes_rel,other_classes_ok)=obtain_rel_dicts(result,numbers,chain_name,current_class)
+                            (show_class,current_poslists,current_motif,other_classes_ok)=traduce_all_poslists_to_ourclass_numb(motifs_dict,gnum_classes_rel,cons_pos_dict,current_class,other_classes_ok)
+                            obtain_predef_positions_lists(current_poslists,current_motif,other_classes_ok,current_class,cons_pos_dict, motifs,gpcr_pdb,gpcr_aa,gnum_classes_rel,multiple_chains,chain_name)
+                motifs_dict_def={"A":[],"B":[],"C":[],"F":[]}
+                find_missing_positions(motifs_dict_def,current_motif,current_poslists,other_classes_ok,current_class,cons_pos_dict,motifs)
+                # for gpcr_class in cons_pos_dict:
+                #     print(gpcr_class)
+                #     for cons_pos_li in cons_pos_dict[gpcr_class]:
+                #         print(cons_pos_li)
+                #     print("\n")
+
+
+                gpcr_pdb_js=json.dumps(gpcr_pdb) 
+                context={
+                    "structure_file":structure_file, 
+                    "structure_name":structure_name , 
+                    "traj_list":traj_list, 
+                    "compounds" : comp_li,
+                    "show_class" : show_class,
+                    "mol_sw" : cons_pos_dict["A"][1],
+                    "cons_classA" : cons_pos_dict["A"][0],
+                    "motifs_def" : motifs_dict_def["A"],
+                    "cons_classB" : cons_pos_dict["B"][0],
+                    "cons_classC" : cons_pos_dict["C"][0],
+                    "cons_classF" : cons_pos_dict["F"][0],
+                    "gpcr_class" : current_class,
+                    "active_class" : active_class,
+                    "chains" : chain_str,
+                    "gpcr_pdb": gpcr_pdb_js}
+                return render(request, 'view/index.html', context)
        #Cannot use gpcr numbering:
         context={
             "structure_file":structure_file, 
@@ -235,3 +396,12 @@ def index(request):
             "gpcr_pdb": "no"}
         return render(request, 'view/index.html', context)
 
+#PROVA:
+def pre_viewer(request):
+    all_dyn=DyndbDynamics.objects.all()
+    dyn_ids=[dyn.id for dyn in all_dyn]
+    dyn_ids.sort()
+    context={
+        "dyn_ids" : dyn_ids
+    }
+    return render(request, 'view/pre_viewer.html', context)
