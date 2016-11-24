41ee0d4c8c2712ec2b205e25814db632e779b3ad
ismaelresp <ismaelresp@telefonica.net>
2016-11-24 18:59:18 +0100

    improved error handling in searchtop.js

--- dynadb/static/dynadb/searchtop.js
+++ dynadb/static/dynadb/searchtop.js
@@ -37,7 +37,17 @@ function searchtop() {
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             $("#pdbchecker1").prop("disabled",false);
-                alert('Error ocurred: PDB file numbering is corrupted (50,51,32); or resid interval is too short.');
+                if (XMLHttpRequest.readyState == 4) {
+                    var responsetext = XMLHttpRequest.responseText;
+
+                    alert(textStatus.substr(0,1).toUpperCase()+textStatus.substr(1)+":\nStatus: " + XMLHttpRequest.textStatus+". "+errorThrown+".\n"+responsetext);
+                }
+                else if (XMLHttpRequest.readyState == 0) {
+                    alert("Connection error. Please, try later and check that your file is not larger than 50 MB.");
+                }
+                else {
+                    alert("Unknown error");
+                }
         }
     });
 }
