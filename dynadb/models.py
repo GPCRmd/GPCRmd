80782fe0d5dfa408860ff65811102e76f1207e90
ismaelresp <ismaelresp@telefonica.net>
2016-12-22 22:20:41 +0100

    django like related_name in DyndbModeledResidues

--- dynadb/models.py
+++ dynadb/models.py
@@ -866,17 +866,17 @@ class DyndbModeledResidues(models.Model):
         (6,'Other Computational Methods')
     )
     id_protein = models.IntegerField(null=True)
-    id_model = models.ForeignKey(DyndbModel,  models.DO_NOTHING, db_column='id_model', related_name='DyndbModeledResidues_id_model_fky',null=True) 
+    id_model = models.ForeignKey(DyndbModel,  models.DO_NOTHING, db_column='id_model', null=True) 
     chain = models.CharField(max_length=1,blank=True, null=False,default='')
     segid = models.CharField(max_length=4,blank=True, null=False,default='')
     resid_from = models.SmallIntegerField()
     resid_to = models.SmallIntegerField()
     seq_resid_from = models.SmallIntegerField()
     seq_resid_to = models.SmallIntegerField()
-    bonded_to_id_modeled_residues = models.ForeignKey('self', models.DO_NOTHING, db_column='bond_to_id_modeled_residues', blank=True, null=True, related_name='DyndbModeledResidues_bond_to_id_modeled_residues_fky')#!!!!
+    bonded_to_id_modeled_residues = models.ForeignKey('self', models.DO_NOTHING, db_column='bond_to_id_modeled_residues', blank=True, null=True, related_name='dyndbmodeledresidues_bond_to_id_modeled_residues')#!!!!
     pdbid = models.CharField(max_length=6, blank=False, null=True)
     source_type = models.SmallIntegerField(choices=SOURCE_TYPE, default=0)
-    template_id_model = models.ForeignKey(DyndbModel, models.DO_NOTHING, db_column='template_id_model', blank=True, null=True, related_name='DyndbModeledResidues_template_id_protein_fky')
+    template_id_model = models.ForeignKey(DyndbModel, models.DO_NOTHING, db_column='template_id_model', blank=True, null=True, related_name='dyndbmodeledresidues_template_id_protein')
 
     class Meta:
         managed = True
