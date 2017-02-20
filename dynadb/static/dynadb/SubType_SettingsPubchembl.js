6dff972b81dad87ac84912c370258f4f57a4a45a
palilsonjmr <palilson.jmr@gmail.com>
2017-02-20 15:38:25 +0100

    TEST data visualization

--- dynadb/static/dynadb/SubType_SettingsPubchembl.js
+++ dynadb/static/dynadb/SubType_SettingsPubchembl.js
@@ -6,21 +6,29 @@ $(document).ready(function(){
 
        $(document).on('change',"[id|=id_form][id$='-bulk_type'],[id|=id_form][id$='-co_type'],[id|=id_form][id$='-co-is_present'],[id|=id_form][id$='-not-is_present']",function(){
            var self=$(this);
+           var cocrist_type=$(this).parents("[id|=molform]").find("[id|=id_form][id$='-co_type']");
            var ret_pubchem =  $(this).parents("[id|=molform]").find("[id|=id_form][id$='retrieve_type_pubchem']");
            var neu_pubchem =  $(this).parents("[id|=molform]").find("[id|=id_form][id$='neutralize_pubchem']");
            var ret_chembl =  $(this).parents("[id|=molform]").find("[id|=id_form][id$='retrieve_type_chembl']");
            var neu_chembl =  $(this).parents("[id|=molform]").find("[id|=id_form][id$='neutralize_chembl']");
            var charge = $(this).parents("[id|=molform]").find("[id|=id_form][id$='net_charge']");
            
-           if ($(charge).val()){
+           if (typeof $(charge).val() !== 'undefined'){
               if ($(charge).val()==0){
                   $(neu_pubchem).prop('checked',true);
                   $(neu_chembl).prop('checked',true);
               }else{
-                  $(neu_pubchem).prop('checked',false);
-                  $(neu_chembl).prop('checked',false);
-              }  
-           }
+                  if (($(cocrist_type).val()!=0 && !$(cocrist_type).val()!=1) || $(cocrist_type).is(':disabled')) {
+                      console.log("not neutralize   "+$(cocrist_type).val())
+                      $(neu_pubchem).prop('checked',false);
+                      $(neu_chembl).prop('checked',false);
+                  } else {
+                      console.log("neutralize type 0 o 1   "+$(cocrist_type).val())
+                      $(neu_pubchem).prop('checked',true);
+                      $(neu_chembl).prop('checked',true);
+                  }
+              }
+            }
 
            console.log($(self).attr('id')+self.prop("disabled"));
 
--- dynadb/views.py
+++ dynadb/views.py
@@ -3837,7 +3837,8 @@ def SMALL_MOLECULEreuseview(request, submission_id, model_id ):
     if qSubPNew.exists() and qSubMolNew.exists() and qSubModNew.exists():
     #if qSubPNew.exists() and qSubMolNew.exists():
         enabled=True
-    qSub=DyndbSubmissionMolecule.objects.exclude(int_id=None).order_by('int_id').exclude(not_in_model=True).filter(submission_id=DyndbSubmissionModel.objects.filter(model_id=model_id).values_list('submission_id',flat=True)[0],molecule_id__dyndbfilesmolecule__id_files__id_file_types=19).annotate(url=F('molecule_id__dyndbfilesmolecule__id_files__url'))
+    qSub=DyndbSubmissionMolecule.objects.exclude(int_id=None).order_by('int_id').exclude(not_in_model=True).filter(submission_id=DyndbSubmissionModel.objects.filter(model_id=model_id).values_list('submission_id',flat=True)[0],molecule_id__dyndbfilesmolecule__id_files__id_file_types=19).annotate(url=F('molecule_id__dyndbfilesmolecule__id_files__url'),urlstd=F('molecule_id__id_compound__std_id_molecule__dyndbfilesmolecule__id_files__url'))
+
     labtypel=[]
     print(qSub)  ######POR AQUI!!!! ORDENAR POR INT_ID LA QUERY qMOL!!! 
     int_id=[]
@@ -3848,9 +3849,11 @@ def SMALL_MOLECULEreuseview(request, submission_id, model_id ):
     imp=[]
     Type=[]
     url=[]
+    urlstd=[]
     for l in qSub:
         url.append(str(l.url).strip())
-        print(l.url)
+        urlstd.append(str(l.urlstd).strip())
+        print("\nstd url ",l.urlstd)
         print(url)
         labtype=l.COMPOUND_TYPE[l.type]
         labtypel.append(labtype) 
@@ -3873,7 +3876,7 @@ def SMALL_MOLECULEreuseview(request, submission_id, model_id ):
     print(alias)
     print(qCOMP)
     print(qMOL)
-    listExtraMolColapse=list(range(len(qCOMP),40))
+    listExtraMolColapse=list(range(max(int_id),40))
     print(listExtraMolColapse)
     fdbSub = dyndb_Submission_Molecule()
     last=int_id0[-1]
@@ -3891,7 +3894,7 @@ def SMALL_MOLECULEreuseview(request, submission_id, model_id ):
 #       print("AQUI", tt,alias)
 
 #    return render(request,'dynadb/SMALL_MOLECULEreuse.html', {'qMOL':qMOL,'labtypel':labtypel,'Type':Type,'imp':imp,'qCOMP':qCOMP,'int_id':int_id,'int_id0':int_id0,'alias':alias,'submission_id':submission_id,'model_id':model_id})
-    return render(request,'dynadb/SMALL_MOLECULE.html', {'url':url,'fdbSub':fdbSub,'qMOL':qMOL,'labtypel':labtypel,'Type':Type,'imp':imp,'qCOMP':qCOMP,'int_id':int_id,'int_id0':int_id0,'last':last,'alias':alias,'submission_id':submission_id,'model_id':model_id,'list':listExtraMolColapse, 'enabled':enabled})
+    return render(request,'dynadb/SMALL_MOLECULE.html', {'url':url,'urlstd':urlstd,'fdbSub':fdbSub,'qMOL':qMOL,'labtypel':labtypel,'Type':Type,'imp':imp,'qCOMP':qCOMP,'int_id':int_id,'int_id0':int_id0,'last':last,'alias':alias,'submission_id':submission_id,'model_id':model_id,'list':listExtraMolColapse, 'enabled':enabled})
 
 def DYNAMICSreuseview(request, submission_id, model_id ):
     if request.method == 'POST':
@@ -10768,7 +10771,7 @@ def SMALL_MOLECULEview(request, submission_id):
            
                    # check whether it's valid:
     else:
-        qSub=DyndbSubmissionMolecule.objects.exclude(int_id=None).order_by('int_id').filter(submission_id=submission_id,molecule_id__dyndbfilesmolecule__id_files__id_file_types=19).annotate(url=F('molecule_id__dyndbfilesmolecule__id_files__url'))
+        qSub=DyndbSubmissionMolecule.objects.exclude(int_id=None).order_by('int_id').filter(submission_id=submission_id,molecule_id__dyndbfilesmolecule__id_files__id_file_types=19).annotate(url=F('molecule_id__dyndbfilesmolecule__id_files__url'),urlstd=F('molecule_id__id_compound__std_id_molecule__dyndbfilesmolecule__id_files__url'))
         if len(qSub)>0:
             labtypel=[]
             print(qSub)  ######POR AQUI!!!! ORDENAR POR INT_ID LA QUERY qMOL!!! 
@@ -10780,8 +10783,10 @@ def SMALL_MOLECULEview(request, submission_id):
             imp=[]
             Type=[]
             url=[]
+            urlstd=[]
             for l in qSub:
                 url.append(str(l.url).strip())
+                urlstd.append(str(l.urlstd).strip())
                 print(l.url)
                 print(url)
                 labtype=l.COMPOUND_TYPE[l.type]
@@ -10802,6 +10807,7 @@ def SMALL_MOLECULEview(request, submission_id):
                 llo=("; ").join(qALIAS.values_list('other_names',flat=True))
                 alias.append(llo) 
                 qCOMP.append(qCOMPtt) 
+            print("STD molecule list of files", urlstd)
             print(alias)
             print(qCOMP)
             print(qMOL)
@@ -10822,7 +10828,7 @@ def SMALL_MOLECULEview(request, submission_id):
 ###  #          print("AQUI", tt,alias)
   
 ###  #       return render(request,'dynadb/SMALL_MOLECULEreuse.html', {'qMOL':qMOL,'labtypel':labtypel,'Type':Type,'imp':imp,'qCOMP':qCOMP,'int_id':int_id,'int_id0':int_id0,'alias':alias,'submission_id':submission_id,'model_id':model_id})
-            return render(request,'dynadb/SMALL_MOLECULE.html', {'url':url,'fdbSub':fdbSub,'qMOL':qMOL,'labtypel':labtypel,'Type':Type,'imp':imp,'qCOMP':qCOMP,'int_id':int_id,'int_id0':int_id0,'alias':alias,'submission_id':submission_id,'list':listExtraMolColapse, 'saved':True})
+            return render(request,'dynadb/SMALL_MOLECULE.html', {'url':url,'urlstd':urlstd,'fdbSub':fdbSub,'qMOL':qMOL,'labtypel':labtypel,'Type':Type,'imp':imp,'qCOMP':qCOMP,'int_id':int_id,'int_id0':int_id0,'alias':alias,'submission_id':submission_id,'list':listExtraMolColapse, 'saved':True})
         else:    
 #        qSub=DyndbSubmissionMolecule.objects.filter(submission_id=DyndbSubmissionModel.objects.filter(model_id=model_id).values_list('submission_id',flat=True)[0]).order_by('int_id')
 #        print(qSub)  ######POR AQUI!!!! ORDENAR POR INT_ID LA QUERY qMOL!!! 
@@ -10994,6 +11000,7 @@ def submission_summaryiew(request,submission_id):
     fdbSubs = dyndb_Submission_Molecule()
 
 #model section
+     
     qModel=DyndbModel.objects.filter(dyndbsubmissionmodel__submission_id=submission_id)
     print("qModel",qModel)
     p=qModel
@@ -11002,6 +11009,7 @@ def submission_summaryiew(request,submission_id):
     TypeM=p.model.MODEL_TYPE[Typeval][1]
     STypeval=p.values()[0]['source_type']
     SType=p.model.SOURCE_TYPE[STypeval][1]
+    model_id=qModel.values_list('id',flat=True)[0]
 
 #dynamics section
 
@@ -11057,7 +11065,7 @@ def submission_summaryiew(request,submission_id):
     
 
 
-    return render(request,'dynadb/SUBMISSION_SUMMARY.html', { 'qPROT':qPROT,'sci_namel':sci_na_codel,'int_id':int_id,'int_id0':int_id0,'alias':alias,'mseq':mseq,'wseq':wseq,'MUTations':MUTations,'submission_id' : submission_id,'urls':urls,'fdbSubs':fdbSubs,'qMOL':qMOL,'labtypels':labtypels,'Types':Types,'imps':imps,'qCOMP':qCOMP,'int_ids':int_ids,'int_ids0':int_ids0,'p':p,'SType':SType,'TypeM':TypeM, 'ddown':ddown,'qDC':qDC, 'dctypel':dctypel, "lcompname":lcompname, 'lcompname':l_ord_mol, 'compl':compl, 'qDS':qDS, 'data':data })
+    return render(request,'dynadb/SUBMISSION_SUMMARY.html', { 'qPROT':qPROT,'sci_namel':sci_na_codel,'int_id':int_id,'int_id0':int_id0,'alias':alias,'mseq':mseq,'wseq':wseq,'MUTations':MUTations,'submission_id' : submission_id,'urls':urls,'fdbSubs':fdbSubs,'qMOL':qMOL,'labtypels':labtypels,'Types':Types,'imps':imps,'qCOMP':qCOMP,'int_ids':int_ids,'int_ids0':int_ids0,'p':p,'SType':SType,'TypeM':TypeM, 'ddown':ddown,'qDC':qDC, 'dctypel':dctypel, "lcompname":lcompname, 'lcompname':l_ord_mol, 'compl':compl, 'qDS':qDS, 'data':data, 'model_id':model_id })
 
 def protein_summaryiew(request,submission_id):
 #protein section
