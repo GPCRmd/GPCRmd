d73ba314fb1d3af42ca1c337c6004685ba2672fb
ismaelresp <ismaelresp@telefonica.net>
2017-03-24 17:19:34 +0100

    added error log capturing for wsgi

--- protwis/settings.py
+++ protwis/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'protwis.custom_middlewares.WsgiLogErrors',
     
 ]
 
