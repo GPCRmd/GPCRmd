6af926f83234fc14d450fb2d6812e9167c5b83dc
ismaelresp <ismaelresp@telefonica.net>
2017-04-12 14:33:07 +0200

    fix open_chembl and open_pubchem

--- dynadb/views.py
+++ dynadb/views.py
@@ -6472,9 +6472,8 @@ def get_compound_info_chembl(request,submission_id):
         except:
             raise  
 
-@login_required
-@user_passes_test_args(is_submission_owner,redirect_field_name=None)           
-def open_pubchem(request,submission_id):
+@login_required       
+def open_pubchem(request):
     if request.method == 'POST':
         if 'cids' in request.POST.keys():
             cids = request.POST['cids'].split(',')
@@ -6484,9 +6483,8 @@ def open_pubchem(request,submission_id):
             query = query[:query.rfind(' OR ')]
             return render(request,'dynadb/open_pubchem.html',{'query':query,'action':'https://www.ncbi.nlm.nih.gov/pccompound/'})
 
-@login_required
-@user_passes_test_args(is_submission_owner,redirect_field_name=None)            
-def open_chembl(request,submission_id):
+@login_required           
+def open_chembl(request):
     chembl_root_url = 'https://www.ebi.ac.uk/chembl'
     chembl_index_php = chembl_root_url+'/index.php'
     chembl_submission_url = chembl_root_url + '/compound/ids'
