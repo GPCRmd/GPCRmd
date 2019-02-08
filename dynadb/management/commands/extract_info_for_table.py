bd207054166baaf34871a2928f7e1c7d036f9083
marionatf <mariona_tf@hotmail.com>
2019-02-08 19:47:28 +0100

    small bug solved

--- dynadb/management/commands/extract_info_for_table.py
+++ dynadb/management/commands/extract_info_for_table.py
@@ -9,23 +9,19 @@ class Command(BaseCommand):
         if not os.path.isdir(out_path):
             os.makedirs(out_path)
         dynobj=DyndbDynamics.objects.all()
-        first=True
         with open(os.path.join(out_path,"simulated_pdbs.csv"),"w") as outfile:
-            if first:
-                first=False
-            else:
-                mywriter = csv.writer(outfile,delimiter=';')
-                mywriter.writerow(["PDB id","Dyn id","is apo"])
-                for dyn in dynobj:
-                    dyn_id=dyn.id
-                    model=DyndbModel.objects.select_related("id_protein","id_complex_molecule").get(dyndbdynamics__id=dyn_id)
-                    pdb_id=model.pdbid
-                    mtype_val=model.type
-                    if mtype_val==1:
-                        is_apo=False
-                    elif mtype_val==0:
-                        is_apo=True
-                    else:
-                        self.stdout.write(self.style.ERROR("Unknown model type for dyn "+str(dyn_id)))
-                        continue
-                    mywriter.writerow([pdb_id,dyn_id,is_apo])
+            mywriter = csv.writer(outfile,delimiter=';')
+            mywriter.writerow(["PDB id","Dyn id","is apo"])
+            for dyn in dynobj:
+                dyn_id=dyn.id
+                model=DyndbModel.objects.select_related("id_protein","id_complex_molecule").get(dyndbdynamics__id=dyn_id)
+                pdb_id=model.pdbid
+                mtype_val=model.type
+                if mtype_val==1:
+                    is_apo=False
+                elif mtype_val==0:
+                    is_apo=True
+                else:
+                    self.stdout.write(self.style.ERROR("Unknown model type for dyn "+str(dyn_id)))
+                    continue
+                mywriter.writerow([pdb_id,dyn_id,is_apo])
